0463a0f04f0d97abc5b6eeef3810500c9d076675
Pemoshi <reo190328@gmail.com>
2024-11-16 15:59:28 +0900

    いらないの消した

--- spring/src/main/java/com/example/sample1app/Sample1appApplication.java
+++ spring/src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -13,12 +13,6 @@ public class Sample1appApplication {
     return "Hello World";
   }
 
-  // @RequestMapping("/men")
-  // public String men() {
-  //   UserEntity user = userRepository.findUserById(0);
-  //   return String.format("ID=%d", user.getId());
-  // }
-
   public static void main(String[] args) {
     SpringApplication.run(Sample1appApplication.class, args);
   }
